dfc72e1a69824a43a430202fb64a267f65d1c08f
Lukas Hartrumpf <lukas.hartrumpf@nerdware.dev>
2023-09-20 15:10:22 +0200

    fix: typo in controller

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (_req, res) {
-  res.send('Hello World')
+  res.send('Hello beautiful World')
 })
 
 app.get('/v2/', function (_req, res) {
